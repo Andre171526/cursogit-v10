0422e2b16eceff771a50a0c21cea71edae9361ec
Andre17 <andreinaglenitza@gmail.com>
2021-09-23 20:41:38 -0300

    Areas GIT: WORKING DIRECTORY

--- scripts/main.js
+++
@@ -1,6 +0,0 @@
-/* Constantes y Variables */
-    const d = document
-    menuLst = d.querySelector('nav .menu')
-    menuBtn = d.querySelector('nav .btn')
-
-    menuBtn.onclick = () => menuLst.classList.toggle('active') ? menuBtn.innerHTML = '&times;' : menuBtn.innerHTML = '&equiv;'
